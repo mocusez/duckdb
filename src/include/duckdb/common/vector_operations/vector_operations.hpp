76ce8b48c9142500178db3fb4043de9905595af5
MocusEZ <mocusz@163.com>
2026-01-01 23:15:32 +0000

    add fmv auto vector parameter

--- src/include/duckdb/common/vector_operations/vector_operations.hpp
+++ src/include/duckdb/common/vector_operations/vector_operations.hpp
@@ -13,6 +13,21 @@
 
 #include <functional>
 
+// Function Multi-Versioning attributes for auto SIMD optimizations
+#if defined(__GNUC__) && !defined(__clang__)
+    #if defined(__x86_64__)
+        #define AUTO_VEC_DISPATCH __attribute__((target_clones("arch=x86-64-v4","arch=x86-64-v3","default")))
+    #elif defined(__aarch64__)
+        #define AUTO_VEC_DISPATCH __attribute__((target_clones("sve2", "dotprod","simd","default")))
+	#elif defined(__riscv)
+		#define AUTO_VEC_DISPATCH __attribute__((target_clones("arch=+v","arch=+zbb","default")))
+    #else
+        #define AUTO_VEC_DISPATCH
+    #endif
+#else
+    #define AUTO_VEC_DISPATCH
+#endif
+
 namespace duckdb {
 class CastFunctionSet;
 struct GetCastFunctionInput;
